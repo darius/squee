4e3822bf3567cfce9ffedf5759aab81d80e574c6
Darius Bacon <darius@wry.me>
2014-06-11 13:51:24 -0700

    update parser for splitting of terp module

--- parse_sans_offsides.py
+++ parse_sans_offsides.py
@@ -3,8 +3,8 @@ A version of the parser setting aside the indent-sensitive part, for now.
 """
 
 from peglet import Parser, join
-from terp import (Constant, Fetch, Actor, Call, Then, Define, Seclude,
-                  Method, global_env, trampoline, final_k)
+from absyntax import Constant, Fetch, Actor, Call, Then, Define, Seclude, Method
+## from top import global_env, trampoline, final_k
 
 parser_grammar = r"""
 program        = _ sequence !.             mk_body
