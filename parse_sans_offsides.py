4ea5f4d8f2ea3042373e1ba20395d7ab064bdaaa
Darius Bacon <darius@wry.me>
2014-12-30 12:54:53 -0800

    cleaner grammar

--- parse_sans_offsides.py
+++ parse_sans_offsides.py
@@ -2,7 +2,7 @@
 A version of the parser setting aside the indent-sensitive part, for now.
 """
 
-from parson import Grammar
+from parson import Grammar, alter
 from absyntax import Constant, Fetch, Actor, Call, Then, Define, Nest, Method
 
 parser_grammar = r"""
@@ -14,25 +14,22 @@ big            : make
                | id '::='_ big                   :Define
                | binsend.
 
-make           : id method_decl '::'_ body       :bind_simple_actor
+make           : id [method_decl '::'_ body      :Method :hug :Actor]
+                                                 :Define
                | id '::'_ actor                  :Define
                |    '::'_ actor.
-actor          : '{'_ method (';'_ method)* '}'_ :mk_actor.
-method         : method_decl ':'_ body           :mk_method.
+actor          : '{'_ method (';'_ method)* '}'_ :hug :Actor.
+method         : method_decl ':'_ body           :Method.
 body           : '{'_ sequence '}'_              :mk_body.
 
-method_decl    : ( id id (id id)*
-                 | opid id
-                 | id)                           :mk_method_decl.
+method_decl    : ( opid id
+                 | (id id)+
+                 | id)                           :unzip.
 
-binsend        : small opt_binmessage            :mk_opt_call.
-opt_binmessage : opid binsend                    :mk_binmessage
-               |                                 :mk_nomessage.
+binsend        : small ([opid small :unzip]      :Call)*.
 
-small          : tiny opt_message                :mk_opt_call.
-opt_message    : (id tiny)+                      :mk_multimessage
-               | id                              :mk_unimessage
-               |                                 :mk_nomessage.
+small          : tiny
+                 ( [((id tiny)+ | id) :unzip]    :Call)?.
 
 tiny           : number                          :Constant
                | string                          :Constant
@@ -40,7 +37,7 @@ tiny           : number                          :Constant
                | block
                | '('_ big ')'_.
 
-block          : ('`' id)* :hug ':'_ body        :mk_block_method :mk_actor.
+block          : ('`' id)* :hug ':'_ body        :mk_block_method :hug :Actor.
 
 id             : /([A-Za-z][_A-Za-z0-9-]*)/   _.
 opid           : /([~!@%&*\-+=|\\<>,?\\\/]+)/ _.
@@ -56,21 +53,9 @@ def mk_block_method(params, body):
     cue = ('of',) + ('and',)*(len(params)-1) if params else ('run',)
     return Method(cue, params, body)
 
-def mk_actor(*methods):      return Actor(methods)
+def mk_body(*exprs): return Nest(reduce(Then, exprs))
 
-def bind_simple_actor(name, (cue, params), expr):
-    return Define(name, Actor((Method(cue, params, expr),)))
-def mk_method((cue, params), expr):
-    return Method(cue, params, expr)
-
-def mk_body(*exprs):         return Nest(reduce(Then, exprs))
-def mk_method_decl(*parts):  return (parts[0::2], parts[1::2])
-
-def mk_opt_call(e, message): return message(e)
-def mk_nomessage():          return lambda e: e
-def mk_unimessage(cue):      return lambda e: Call(e, (cue,), ())
-def mk_binmessage(opid, arg):return lambda e: Call(e, (opid,), (arg,))
-def mk_multimessage(*args):  return lambda e: Call(e, args[0::2], args[1::2])
+unzip = alter(lambda *parts: (parts[0::2], parts[1::2]))
 
 parse = Grammar(parser_grammar)(**globals()).program
 
@@ -122,4 +107,4 @@ push of element on stack ::
 
 ## sets = open('sets.squee').read()
 ## parse(sets)
-#. ({empty ::= {('adjoin',) ('k',): {(adjoining of k to empty)}; ('has',) ('k',): {no}; ('is-empty',): {yes}; ('merge',) ('s',): {s}}; adjoining ::= {('of', 'to') ('n', 's'): {((s has n) if-so {('run',): {s}} if-not {('run',): {extension ::= {('adjoin',) ('k',): {(adjoining of k to extension)}; ('has',) ('k',): {(n == (k || {('run',): {(s has k)}}))}; ('is-empty',): {no}; ('merge',) ('t',): {(merging of extension with t)}}}})}}; merging ::= {('of', 'with') ('s1', 's2'): {meld ::= {('adjoin',) ('k',): {(adjoining of k to meld)}; ('has',) ('k',): {((s1 has k) || {('run',): {(s2 has k)}})}; ('is-empty',): {((s1 is-empty) && {('run',): {(s2 is-empty)}})}; ('merge',) ('s',): {(merging of meld with s)}}}}; (make-list of (empty has 42) and ((empty adjoin 42) has 42))},)
+#. ({empty ::= {('adjoin',) ('k',): {(adjoining of k to empty)}; ('has',) ('k',): {no}; ('is-empty',): {yes}; ('merge',) ('s',): {s}}; adjoining ::= {('of', 'to') ('n', 's'): {((s has n) if-so {('run',): {s}} if-not {('run',): {extension ::= {('adjoin',) ('k',): {(adjoining of k to extension)}; ('has',) ('k',): {((n = k) || {('run',): {(s has k)}})}; ('is-empty',): {no}; ('merge',) ('t',): {(merging of extension with t)}}}})}}; merging ::= {('of', 'with') ('s1', 's2'): {meld ::= {('adjoin',) ('k',): {(adjoining of k to meld)}; ('has',) ('k',): {((s1 has k) || {('run',): {(s2 has k)}})}; ('is-empty',): {((s1 is-empty) && {('run',): {(s2 is-empty)}})}; ('merge',) ('s',): {(merging of meld with s)}}}}; (make-list of (empty has 42) and ((empty adjoin 42) has 42))},)
